4152e993ff9c9c4e52199688e694a306875e994c
Ivan Matviyenko <Ivan.Matviyenko@kcell.kz>
2018-10-05 00:51:10 +0600

    Fix TokenHelper - all cert private key accesses with elevated privileges and calculate correct exp claim value

--- UPSBrowser/Util/TokenHelper.cs
+++ UPSBrowser/Util/TokenHelper.cs
@@ -25,7 +25,7 @@ namespace Kcell.UPSBrowser
             // In .NET 4.5 which is the target framework version, DateTimeOffset does not have the ToUnixTimeSeconds method which was only introduced in .NET 4.6
             var dateNowUtc = DateTime.UtcNow;
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            var unixDateTime = (dateNowUtc - epoch).TotalSeconds;
+            var unixDateTime = (dateNowUtc - epoch).TotalSeconds + (Constants.jwtTokenLifetimeInMinutes * 60);
 
 
             var payload = new Dictionary<string, object>()
@@ -37,29 +37,34 @@ namespace Kcell.UPSBrowser
                 //{ "exp", DateTimeOffset.UtcNow.AddMinutes(Constants.jwtTokenLifetimeInMinutes).ToUnixTimeSeconds() }
                 { "exp", unixDateTime }
             };
-
-            // If you get "Keyset does not exist" exception at this stage, make sure the the SP web app pool account has access to the private key of the selected cert
-            UPSBrowserLogger.LogDebug(loggingCategory, "Trying to get the cert's private key...");
-            var rsaCryptoServiceProvider = signingCertificate.cert.PrivateKey as RSACryptoServiceProvider;
-
             string token = null;
-            try
+
+
+            SPSecurity.RunWithElevatedPrivileges(delegate ()
             {
-                UPSBrowserLogger.LogDebug(loggingCategory, "Trying to generate a JWT token string using the private key...");
-                token = Jose.JWT.Encode(payload, rsaCryptoServiceProvider, JwsAlgorithm.RS256);
-            } catch (System.Security.Cryptography.CryptographicException cryptoException)
-            {
-                UPSBrowserLogger.LogDebug(loggingCategory, "System.Security.Cryptography.CryptographicException catched");
+                UPSBrowserLogger.LogDebug(loggingCategory, "Running with elevated privileges");
 
-                // Look for "Invalid algorithm specified" exception - 
-                UPSBrowserLogger.LogInfo(loggingCategory, $"cryptoException.Message: {cryptoException.Message}");
+                // If you get "Keyset does not exist" exception at this stage, make sure the the SP web app pool account has access to the private key of the selected cert
+                UPSBrowserLogger.LogDebug(loggingCategory, "Trying to get the cert's private key...");
+                var rsaCryptoServiceProvider = signingCertificate.cert.PrivateKey as RSACryptoServiceProvider;
 
-                var privateKey = signingCertificate.cert.PrivateKey as RSACryptoServiceProvider;
-                bool privateKeyIsExportable = privateKey.CspKeyContainerInfo.Exportable;
-
-                if (privateKeyIsExportable)
+                
+                try
                 {
-                    SPSecurity.RunWithElevatedPrivileges(delegate ()
+                    UPSBrowserLogger.LogDebug(loggingCategory, "Trying to generate a JWT token string using the private key...");
+                    token = Jose.JWT.Encode(payload, rsaCryptoServiceProvider, JwsAlgorithm.RS256);
+                }
+                catch (System.Security.Cryptography.CryptographicException cryptoException)
+                {
+                    UPSBrowserLogger.LogDebug(loggingCategory, "System.Security.Cryptography.CryptographicException catched");
+
+                    // Look for "Invalid algorithm specified" exception - 
+                    UPSBrowserLogger.LogInfo(loggingCategory, $"cryptoException.Message: {cryptoException.Message}");
+
+                    var privateKey = signingCertificate.cert.PrivateKey as RSACryptoServiceProvider;
+                    bool privateKeyIsExportable = privateKey.CspKeyContainerInfo.Exportable;
+
+                    if (privateKeyIsExportable)
                     {
                         UPSBrowserLogger.LogDebug(loggingCategory, $"Recreating RsaCryptoServiceProvider using the same cert with MS Enhanced CSP to enable SHA256");
 
@@ -73,13 +78,16 @@ namespace Kcell.UPSBrowser
 
                         UPSBrowserLogger.LogDebug(loggingCategory, "Trying to generate a JWT token string again using the reimported private key...");
                         token = Jose.JWT.Encode(payload, rsaCryptoServiceProvider_MSEnchancedCSP, JwsAlgorithm.RS256);
-                    });
-                } else
-                {
-                    UPSBrowserLogger.LogError(loggingCategory, $"Cannot recreate RsaCryptoServiceProvider with MS Enhanced CSP, the original cert private key is not exportable");
-                    token = null;
-                }
-            };
+                    }
+                    else
+                    {
+                        UPSBrowserLogger.LogError(loggingCategory, $"Cannot recreate RsaCryptoServiceProvider with MS Enhanced CSP, the original cert private key is not exportable");
+                        token = null;
+                    }
+                };
+            });
+
+
 
             UPSBrowserLogger.LogDebug(loggingCategory, $"token: {token}");
             return token;
